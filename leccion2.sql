0b236824d00b915884e0c6b8939f05b957e9c296
maria <mpramos78915400@gmail.com>
2024-06-01 11:39:42 -0400

    leccion 1 select

---
+++ leccion2.sql
@@ -0,0 +1,19 @@
+--**Ejercicio 3: Tareas**Toy Story 2
+
+--1. Encuentra todas las películas de Toy Story
+select * from movie
+select *
+from movie
+where titulo like 'Toy Story%'
+--2. Encuentra todas las películas dirigidas por John Lasseter
+select *
+from movie
+where director ='Juan Lasseter'
+--3. Encuentra todas las películas (y directores) no dirigidas por John Lasseter
+select *
+from movie
+where director !='Juan Lasseter'
+--4. Encuentra todas las películas de WALL-*
+select *
+from movie
+where titulo like 'WALL-%'

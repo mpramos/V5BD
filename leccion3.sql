a9878d7db8457a2cd3bfd4c1f0f421c9e4a0f7dd
maria <mpramos78915400@gmail.com>
2024-06-01 12:01:24 -0400

    leccion 3 distinct|order by, limit

---
+++ leccion3.sql
@@ -0,0 +1,13 @@
+--Ejercicio 4: Tareas
+
+--1. Enumere todos los directores de las películas de Pixar (alfabéticamente), sin duplicados
+select * from movie
+select distinct director
+from movie 
+order by director asc
+--2. Enumere las últimas cuatro películas de Pixar estrenadas (ordenadas de más reciente a menos) 
+
+select *
+from movie
+order by anio desc
+limit 4

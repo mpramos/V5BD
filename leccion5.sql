e4c1da239c76bd531fe5911efa35e74cb5238f1f
maria <mpramos78915400@gmail.com>
2024-06-05 20:41:29 -0400

    Leccion 6

---
+++ leccion5.sql
@@ -0,0 +1,14 @@
+--! EJERCICIOS 
+
+--? Ejercicio 6: Tareas
+
+-- 1. Encuentra las ventas nacionales e internacionales de cada película.
+select M.titulo,T.ventas_locales,T.ventas_internacionales
+from movie as M
+join Taquillas as T on M.identificacion=T.id_pelicula
+-- 2. Muestre las cifras de ventas de cada película que obtuvo mejores resultados a nivel internacional que a nivel nacional.
+select M.titulo , T.ventas_internacionales, T.ventas_locales
+from movie as M
+join Taquillas as T on M.identificacion=T.id_pelicula
+where T.ventas_internacionales > T.ventas_locales
+-- 3. Enumere todas las películas por clasificación en orden descendente.

033cd8ce2f280003bc323f40a8b693033835f4d5
maria <mpramos78915400@gmail.com>
2024-06-05 21:34:25 -0400

    leccion6

--- leccion6.sql
+++ leccion6.sql
@@ -1,5 +1,15 @@
 -- 1. Encuentre la lista de todos los edificios que tienen empleados.
-
+select distinct E.Nombre_Del_Edificio
+from edificio as E
+left join empleado as Emp
+on E.Nombre_Del_Edificio = Emp.edificio
 -- 2. Encuentra la lista de todos los edificios y su capacidad.
-
--- 3. Enumere todos los edificios y las distintas funciones de los empleados en cada edificio (incluidos los edificios vacíos)
+select distinct E.Nombre_Del_Edificio, E.capacidad
+from edificio as E
+left join empleado as Emp
+on E.Nombre_Del_Edificio = Emp.edificio
+-- 3. Enumere todos los edificios y las distintas funciones
+--  de los empleados en cada edificio (incluidos los edificios vacíos)
+select E.nombre_del_edificio, coalesce(Empl.role,'SIN EMPLEADOS')as funcion_Empleado
+from edificio E
+left join empleado as Empl on E.Nombre_Del_edificio = Empl.edificio

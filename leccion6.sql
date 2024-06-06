78ee62fccbac4d09c67cb32244e4b319362cd1ad
maria <mpramos78915400@gmail.com>
2024-06-05 21:14:24 -0400

    tabla de empleados

---
+++ leccion6.sql
@@ -0,0 +1,5 @@
+-- 1. Encuentre la lista de todos los edificios que tienen empleados.
+
+-- 2. Encuentra la lista de todos los edificios y su capacidad.
+
+-- 3. Enumere todos los edificios y las distintas funciones de los empleados en cada edificio (incluidos los edificios vacíos)
--- tables.sql
+++ tables.sql
@@ -47,4 +47,27 @@ values
         (2 , 7.2 , 162798565 , 200600000),
         (13 , 7.2 , 237283207 , 301700000)
 select * from Taquillas
+create table Empleados(
+    Role TEXT,
+    Nombree TEXT,
+    Edifico TEXT,
+    Años_empleado TEXT
+)
+INSERT INTO Empleados (Role, Nombre, Edificio, Años_empleado) VALUES ('Ingeniero', 'Becky A.', '1e', 4);
+INSERT INTO Empleados (Role, Nombre, Edificio, Años_empleado) VALUES ('Ingeniero', 'Dan B.', '1e', 2);
+INSERT INTO Empleados (Role, Nombre, Edificio, Años_empleado) VALUES ('Ingeniero', 'Sharon F.', '1e', 6);
+INSERT INTO Empleados (Role, Nombre, Edificio, Años_empleado) VALUES ('Ingeniero', 'Dan M.', '1e', 4);
+INSERT INTO Empleados (Role, Nombre, Edificio, Años_empleado) VALUES ('Ingeniero', 'Malcom S.', '1e', 1);
+INSERT INTO Empleados (Role, Nombre, Edificio, Años_empleado) VALUES ('Artista', 'tylar s.', '2w', 2);
+INSERT INTO Empleados (Role, Nombre, Edificio, Años_empleado) VALUES ('Artista', 'Sherman D.', '2w', 8);
+INSERT INTO Empleados (Role, Nombre, Edificio, Años_empleado) VALUES ('Artista', 'Jacob J.', '2w', 6);
+INSERT INTO Empleados (Role, Nombre, Edificio, Años_empleado) VALUES ('Artista', 'Lilia A.', '2w', 7);
+INSERT INTO Empleados (Role, Nombre, Edificio, Años_empleado) VALUES ('Artista', 'Brandon J.', '2w', 7);
+INSERT INTO Empleados (Role, Nombre, Edificio, Años_empleado) VALUES ('Gerente', 'Scott K.', '1e', 9);
+INSERT INTO Empleados (Role, Nombre, Edificio, Años_empleado) VALUES ('Gerente', 'Shirlee M.', '1e', 3);
+INSERT INTO Empleados (Role, Nombre, Edificio, Años_empleado) VALUES ('Gerente', 'Daria O.', '2w', 6);
+INSERT INTO Empleados (Role, Nombre, Edificio, Años_empleado) VALUES ('Gerente', 'Daria O.', '2w', 6);
+INSERT INTO Empleados (Role, Nombre, Edificio, Años_empleado) VALUES ('Gerente', 'Juana O.', NULL, 6);
+
+
 

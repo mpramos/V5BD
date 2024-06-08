ea559f0c4cfcdb1934f2bdf4c70563a668d264d3
maria <mpramos78915400@gmail.com>
2024-06-08 11:18:18 -0400

    finalizacion de la leccion 8

--- leccion8.sql
+++ leccion8.sql
@@ -6,4 +6,15 @@ from movie as M
 join taquillas as T	on M.identificacion =T.id_pelicula
 
 -- 2. Enumere todas las películas y sus calificaciones **en porcentaje.**
+SELECT M.titulo AS Pelicula,T.clasificacion * 10 AS Calificacion_Porcentaje
+FROM 
+    Movie as M
+JOIN 
+    Taquillas as T
+ON 
+    M.Identificacion = T.id_pelicula;
+
 -- 3. Enumere todas las películas que se estrenaron en años pares.
+SELECT titulo, anio as Años_pares
+FROM Movie 
+WHERE anio % 2 = 0;

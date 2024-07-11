d9410e05b9ff6316f7a49c2fe301128016329721
maria <mpramos78915400@gmail.com>
2024-07-10 20:48:09 -0400

    procedimientos

--- ProcedimientosAlmacenados.sql
+++ ProcedimientosAlmacenados.sql
@@ -1,9 +1,14 @@
-create table empleadosP (
+create table if not exists empleados (
     id serial primary key,
     nombre VARCHAR(50),
     puesto VARCHAR(50),
-    salario NUMERIC(10,2)
+    salario NUMERIC(10,2),
+    departamento VARCHAR(50)
 );
+INSERT INTO empleados( nombre,puesto,salario, departamento) values
+('Juan Perez','Desarrollador', 75000.00,'Desarrollo'),
+('Ana Perez','Desarrollador', 85000.00,'Administracion'),
+('Pedro  Perez','Desarrollador', 95000.00,'Desarrollo');
 
 create or replace procedure insertar_empleado(
     nombre VARCHAR,
@@ -18,7 +23,23 @@ BEGIN
 END;
 $$;
 CALL insertar_empleado('Juan Pérez', 'Desarrollador', 7500)
+CALL insertar_empleado('Carlos López', 'Gerente', 7500)
+CALL insertar_empleado('Laura Sánches', 'Diseñadora', 7500)
 
 select * from empleadosP
 
 
+create or replace procedure aumentar_salario_departamento(
+    departamento VARCHAR,
+    porcentaje NUMERIC
+)
+LANGUAGE plpgsql
+AS $$
+BEGIN   
+    UPDATE empleadosP
+    SET salario = salario +(salario*(porcentaje/100))
+    where departamento=departamento;
+END;
+$$    
+
+CALL aumentar_salario_departamento('Desarrollo',10); 
